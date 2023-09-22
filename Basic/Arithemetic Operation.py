005ec4bccc861c529d74493326e4aed358e74b7f
Saniya Bubere <116152917+SaniyaBubere@users.noreply.github.com>
2023-09-22 14:54:27 +0530

    Create Arithemetic Operation.py

---
+++ Basic/Arithemetic Operation.py
@@ -0,0 +1,32 @@
+'''Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.'''
+
+# Solution
+
+# Read an integer 'a' from the user
+a = int(input())
+
+# Read another integer 'b' from the user
+b = int(input())
+
+# Perform addition of 'a' and 'b' and store the result in 'c'
+c = a + b
+
+# Perform subtraction of 'a' and 'b' and store the result in 'd'
+d = a - b
+
+# Perform multiplication of 'a' and 'b' and store the result in 'e'
+e = a * b
+
+# Print the result of addition ('c')
+print(c)
+
+# Print the result of subtraction ('d')
+print(d)
+
+# Print the result of multiplication ('e')
+print(e)
